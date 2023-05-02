dea2514dc21ed5a957039d2ba9f772ff17b7c46f
wasiq-rai <wasiqrai1122@gmail.com>
2023-05-02 23:59:25 +0500

    Commit

--- .vscode/one-csv-to-other-csv.py
+++ .vscode/one-csv-to-other-csv.py
@@ -14,3 +14,20 @@ def read_data(filename):
                 'income': int(row[3])
             })
         return data
+# Define a function to calculate some statistics on the data
+def analyze_data(data):
+    num_female = 0
+    num_male = 0
+    total_income = 0
+    for row in data:
+        if row['gender'] == 'Female':
+            num_female += 1
+        elif row['gender'] == 'Male':
+            num_male += 1
+        total_income += row['income']
+    avg_income = total_income / len(data)
+    return {
+        'num_female': num_female,
+        'num_male': num_male,
+        'avg_income': avg_income
+    }
